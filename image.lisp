366a095f3cb847e0d15cfed146bbfbb765b6fa4a
Julian Squires <julian@cipht.net>
2015-02-05 09:41:52 -0500

    Fix image return types

--- image.lisp
+++ image.lisp
@@ -1,5 +1,5 @@
 
-(in-package #:net.cipht/sdl2-image)
+(in-package :net.cipht/sdl2-image)
 
 (define-foreign-library sdl-image
   (:windows "SDL2_image")
@@ -11,7 +11,7 @@
   (:init-png #x2)
   (:init-tif #x4)
   (:init-webp #x8))
-(defcfun ("IMG_Init" %init) :boolean (flags init-flags))
+(defcfun ("IMG_Init" %init) sdl:success? (flags init-flags))
 (defcfun ("IMG_Quit" quit) :void)
 
 (defun init (flags) (%init (sdl::make-bitfield 'init-flags flags)))
@@ -20,17 +20,17 @@
   `(unwind-protect (progn (init ,mask) ,@body)
      (quit)))
 
-(defcfun ("IMG_Load" %load) (:pointer (:struct sdl::surface)) (path :string))
-(defun load (path) (sdl::maybe-null-ptr (%load path)))
+(defcfun ("IMG_Load" %load) (:pointer (:struct sdl:surface)) (path :string))
+(defun load (path) (sdl::nil<-null (%load path)))
 
 (defcfun ("IMG_LoadTexture" %load-texture)
-    sdl::texture
-  (renderer sdl::renderer)
+    sdl:texture
+  (renderer sdl:renderer)
   (path :string))
 (defun load-texture (renderer path)
-  (sdl::maybe-null-ptr (%load-texture renderer path)))
+  (sdl::nil<-null (%load-texture renderer path)))
 
 (defcfun ("IMG_SavePNG" save-png)
-    :int
-  (surface (:pointer (:struct sdl::surface)))
+    sdl:success?
+  (surface (:pointer (:struct sdl:surface)))
   (path :string))
